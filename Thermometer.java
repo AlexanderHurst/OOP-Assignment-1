9129bbcb1c4632de64172c2b8704bec229a7057b
Alexander Hurst <alexander@AlexanderLinux.oncampus.local>
2017-10-03 17:31:20 -0400

    added gitignore file

--- Thermometer.java
+++ Thermometer.java
@@ -2,6 +2,7 @@ public class Thermometer{
     public static void main(String[] args) {
         PrintVoltageWater VoltageWater = new PrintVoltageWater (20,75,50,0.5);
         System.out.println("T\tvm");
+        System.out.println ("-----\t-----");
         for (double T = 0; T<=100; T+=10){
             System.out.println ((int)(T) + "\t" + twoDecimals(VoltageWater.calcVm(T)));
         }
